ecf58d1bcc31ba2d7cfd38c5ef607950a88890c3
Linux User <geeknix@sailor-jupiter.hsd1.wa.comcast.net>
2024-05-15 11:10:09 -0700

    update lib name

--- duper-extract.sh
+++ duper-extract.sh
@@ -5,58 +5,58 @@ sleep 120
 # Install WhatsApp normally and extract Arm64 libraries
 /Users/runner/Library/Android/sdk/platform-tools/adb root
 
-# Replace libs.spk.zst with arm64-v8a libs.spk.zst 
+# Replace libs.spo with arm64-v8a libs.spo 
 apktool d -rs WhatsApp.apk
-cp WhatsApp/assets/compressed/arm64-v8a/libs.spk.zst WhatsApp/assets/compressed/armeabi-v7a/.
-cp WhatsApp/assets/compressed/arm64-v8a/libs.spk.zst WhatsApp/assets/compressed/x86/.
-cp WhatsApp/assets/compressed/arm64-v8a/libs.spk.zst WhatsApp/assets/compressed/x86_64/.
+cp WhatsApp/assets/compressed/arm64-v8a/libs.spo WhatsApp/assets/compressed/armeabi-v7a/.
+cp WhatsApp/assets/compressed/arm64-v8a/libs.spo WhatsApp/assets/compressed/x86/.
+cp WhatsApp/assets/compressed/arm64-v8a/libs.spo WhatsApp/assets/compressed/x86_64/.
 apktool b WhatsApp
 java -jar ./uber-apk-signer-1.3.0.jar -a ./WhatsApp/dist/WhatsApp.apk && /Users/runner/Library/Android/sdk/platform-tools/adb install WhatsApp/dist/WhatsApp-aligned-debugSigned.apk
 rm -rf ./WhatsApp
 /Users/runner/Library/Android/sdk/platform-tools/adb shell am start -n com.whatsapp/.Main && sleep 120
-/Users/runner/Library/Android/sdk/platform-tools/adb pull /data/data/com.whatsapp/files/decompressed/libs.spk.zst/ . && zip -r libs.spk.zst.arm64.zip libs.spk.zst/
-rm -rf ./libs.spk.zst
+/Users/runner/Library/Android/sdk/platform-tools/adb pull /data/data/com.whatsapp/files/decompressed/libs.spo/ . && zip -r libs.spo.arm64.zip libs.spo/
+rm -rf ./libs.spo
 /Users/runner/Library/Android/sdk/platform-tools/adb shell rm -rf /data/data/com.whatsapp
 /Users/runner/Library/Android/sdk/platform-tools/adb uninstall com.whatsapp
 
-# Replace libs.spk.zst with x86 libs.spk.zst 
+# Replace libs.spo with x86 libs.spo 
 apktool d -rs WhatsApp.apk
-cp WhatsApp/assets/compressed/x86/libs.spk.zst WhatsApp/assets/compressed/arm64-v8a/.
-cp WhatsApp/assets/compressed/x86/libs.spk.zst WhatsApp/assets/compressed/armeabi-v7a/.
-cp WhatsApp/assets/compressed/x86/libs.spk.zst WhatsApp/assets/compressed/x86_64/.
+cp WhatsApp/assets/compressed/x86/libs.spo WhatsApp/assets/compressed/arm64-v8a/.
+cp WhatsApp/assets/compressed/x86/libs.spo WhatsApp/assets/compressed/armeabi-v7a/.
+cp WhatsApp/assets/compressed/x86/libs.spo WhatsApp/assets/compressed/x86_64/.
 apktool b WhatsApp
 java -jar ./uber-apk-signer-1.3.0.jar -a ./WhatsApp/dist/WhatsApp.apk && /Users/runner/Library/Android/sdk/platform-tools/adb install WhatsApp/dist/WhatsApp-aligned-debugSigned.apk
 rm -rf ./WhatsApp
 /Users/runner/Library/Android/sdk/platform-tools/adb shell am start -n com.whatsapp/.Main && sleep 120
-/Users/runner/Library/Android/sdk/platform-tools/adb pull /data/data/com.whatsapp/files/decompressed/libs.spk.zst/ . && zip -r libs.spk.zst.x86.zip libs.spk.zst/
-rm -rf ./libs.spk.zst
+/Users/runner/Library/Android/sdk/platform-tools/adb pull /data/data/com.whatsapp/files/decompressed/libs.spo/ . && zip -r libs.spo.x86.zip libs.spo/
+rm -rf ./libs.spo
 /Users/runner/Library/Android/sdk/platform-tools/adb shell rm -rf /data/data/com.whatsapp
 /Users/runner/Library/Android/sdk/platform-tools/adb uninstall com.whatsapp
 
-# Replace libs.spk.zst with x86_64 libs.spk.zst 
+# Replace libs.spo with x86_64 libs.spo 
 apktool d -rs WhatsApp.apk
-cp WhatsApp/assets/compressed/x86_64/libs.spk.zst WhatsApp/assets/compressed/arm64-v8a/.
-cp WhatsApp/assets/compressed/x86_64/libs.spk.zst WhatsApp/assets/compressed/armeabi-v7a/.
-cp WhatsApp/assets/compressed/x86_64/libs.spk.zst WhatsApp/assets/compressed/x86/.
+cp WhatsApp/assets/compressed/x86_64/libs.spo WhatsApp/assets/compressed/arm64-v8a/.
+cp WhatsApp/assets/compressed/x86_64/libs.spo WhatsApp/assets/compressed/armeabi-v7a/.
+cp WhatsApp/assets/compressed/x86_64/libs.spo WhatsApp/assets/compressed/x86/.
 apktool b WhatsApp
 java -jar ./uber-apk-signer-1.3.0.jar -a ./WhatsApp/dist/WhatsApp.apk && /Users/runner/Library/Android/sdk/platform-tools/adb install WhatsApp/dist/WhatsApp-aligned-debugSigned.apk
 rm -rf ./WhatsApp
 /Users/runner/Library/Android/sdk/platform-tools/adb shell am start -n com.whatsapp/.Main && sleep 120
-/Users/runner/Library/Android/sdk/platform-tools/adb pull /data/data/com.whatsapp/files/decompressed/libs.spk.zst/ . && zip -r libs.spk.zst.x86_64.zip libs.spk.zst/
-rm -rf ./libs.spk.zst
+/Users/runner/Library/Android/sdk/platform-tools/adb pull /data/data/com.whatsapp/files/decompressed/libs.spo/ . && zip -r libs.spo.x86_64.zip libs.spo/
+rm -rf ./libs.spo
 /Users/runner/Library/Android/sdk/platform-tools/adb shell rm -rf /data/data/com.whatsapp
 /Users/runner/Library/Android/sdk/platform-tools/adb uninstall com.whatsapp
 
-# Replace libs.spk.zst with armeabi-v7a libs.spk.zst 
+# Replace libs.spo with armeabi-v7a libs.spo 
 apktool d -rs WhatsApp.apk
-cp WhatsApp/assets/compressed/armeabi-v7a/libs.spk.zst WhatsApp/assets/compressed/arm64-v8a/.
-cp WhatsApp/assets/compressed/armeabi-v7a/libs.spk.zst WhatsApp/assets/compressed/x86/.
-cp WhatsApp/assets/compressed/armeabi-v7a/libs.spk.zst WhatsApp/assets/compressed/x86_64/.
+cp WhatsApp/assets/compressed/armeabi-v7a/libs.spo WhatsApp/assets/compressed/arm64-v8a/.
+cp WhatsApp/assets/compressed/armeabi-v7a/libs.spo WhatsApp/assets/compressed/x86/.
+cp WhatsApp/assets/compressed/armeabi-v7a/libs.spo WhatsApp/assets/compressed/x86_64/.
 apktool b WhatsApp
 java -jar ./uber-apk-signer-1.3.0.jar -a ./WhatsApp/dist/WhatsApp.apk && /Users/runner/Library/Android/sdk/platform-tools/adb install WhatsApp/dist/WhatsApp-aligned-debugSigned.apk
 rm -rf ./WhatsApp
 /Users/runner/Library/Android/sdk/platform-tools/adb shell am start -n com.whatsapp/.Main && sleep 120
-/Users/runner/Library/Android/sdk/platform-tools/adb pull /data/data/com.whatsapp/files/decompressed/libs.spk.zst/ . && zip -r libs.spk.zst.armeabi-v7a.zip libs.spk.zst/
-rm -rf ./libs.spk.zst
+/Users/runner/Library/Android/sdk/platform-tools/adb pull /data/data/com.whatsapp/files/decompressed/libs.spo/ . && zip -r libs.spo.armeabi-v7a.zip libs.spo/
+rm -rf ./libs.spo
 /Users/runner/Library/Android/sdk/platform-tools/adb shell rm -rf /data/data/com.whatsapp
 /Users/runner/Library/Android/sdk/platform-tools/adb uninstall com.whatsapp
